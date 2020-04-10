69a1779200b52da45de21b1cea3d9ab690844615
minhho98langlon <37680096+minhho98langlon@users.noreply.github.com>
2020-04-10 07:24:21 +0700

    Add migration file

---
+++ database/migrations/2020_04_10_000610_create_song_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSongTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('songs', function (Blueprint $table) {
+            $table->bigIncrements('id')->autoIncrement();
+            $table->string('wid', 20)->unique();
+            $table->string('name');
+            $table->text('links');
+            $table->text('lyric');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('songs');
+    }
+}
